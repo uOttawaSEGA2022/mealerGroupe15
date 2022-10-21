9a003f70f1ce72b768055b695c71304cde774aa4
MorFall <papemorsylla2201@gmail.com>
2022-10-21 01:34:04 -0400

    Creation de Activity_sign_up

---
+++ app/src/main/java/com/example/mealer/Activity_sign_up.java
@@ -0,0 +1,17 @@
+package com.example.mealer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Activity_sign_up extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
