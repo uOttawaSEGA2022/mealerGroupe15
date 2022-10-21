095f7390c6dd4108a513231ae31f623700e68126
MorFall <papemorsylla2201@gmail.com>
2022-10-21 02:48:21 -0400

    mor

--- app/src/main/java/com/example/mealer/SignUpActivity.java
+++ app/src/main/java/com/example/mealer/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void OnClickReturnEntantque(View view) {
+        Intent intent = new Intent(getApplicationContext(), Activity2.class);
+        startActivityForResult(intent, 0);
     }
 }
