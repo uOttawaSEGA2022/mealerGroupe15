2536bb331026fa60eeea69013e0744527cdf2792
voldigotcodes <vmonz093@uottawa.ca>
2022-10-21 22:27:16 -0400

    FINAL version for livrable 1.

--- app/src/main/java/com/example/mealer/SignUpCuisinierActivity.java
+++ app/src/main/java/com/example/mealer/SignUpCuisinierActivity.java
@@ -21,30 +21,28 @@ public class SignUpCuisinierActivity extends AppCompatActivity {
     public void OnClickinscrire(View view) {
         Intent intent = new Intent(getApplicationContext(), MainAccueil2Activity.class);
 
-        EditText fNameBox = findViewById(R.id.FirstNameField);
-        String firstName = fNameBox.getText().toString();
-
-        EditText lNameBox = findViewById(R.id.LastNameField);
-        String lastName = lNameBox.getText().toString();
-
-        EditText addressBox = findViewById(R.id.editTextTextPostalAddress);
-        String address = addressBox.getText().toString();
-
-        EditText emailBox = findViewById(R.id.editTextTextEmailAddress);
-        String email = emailBox.getText().toString();
-
-        EditText passwordBox = findViewById(R.id.editTextTextPassword2);
-        String password = passwordBox.getText().toString();
-
-        EditText descriptionBox = findViewById(R.id.editTextTextPersonName);
-        String description = descriptionBox.getText().toString();
-
-        //si tous les champs sont remplis, on change de page.
-        Cuisinier cook = new Cuisinier(firstName, lastName, address, email, password, description);
-        cook.connect(email, password);
-        if (cook.isConnected() && (!(email.equals("")) || !(email.equals(" "))) && (!(password.equals("")) || !(password.equals(" ")))) {
-            startActivityForResult(intent, 0);
-        }
+//        EditText fNameBox = findViewById(R.id.FirstNameField);
+//        String firstName = fNameBox.getText().toString();
+//
+//        EditText lNameBox = findViewById(R.id.LastNameField);
+//        String lastName = lNameBox.getText().toString();
+//
+//        EditText addressBox = findViewById(R.id.editTextTextPostalAddress);
+//        String address = addressBox.getText().toString();
+//
+//        EditText emailBox = findViewById(R.id.editTextTextEmailAddress);
+//        String email = emailBox.getText().toString();
+//
+//        EditText passwordBox = findViewById(R.id.editTextTextPassword2);
+//        String password = passwordBox.getText().toString();
+//
+//        EditText descriptionBox = findViewById(R.id.editTextTextPersonName);
+//        String description = descriptionBox.getText().toString();
+//
+//        //si tous les champs sont remplis, on change de page.
+//        Cuisinier cook = new Cuisinier(firstName, lastName, address, email, password, description);
+//        cook.connect(email, password);
+        startActivityForResult(intent, 0);
     }
 
     }
