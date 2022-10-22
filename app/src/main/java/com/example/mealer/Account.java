f705fea70a35fb0993011221eeb31b976c96dfef
voldigotcodes <vmonz093@uottawa.ca>
2022-10-21 22:38:09 -0400

    FINAL version for livrable 1.

--- app/src/main/java/com/example/mealer/Account.java
+++ app/src/main/java/com/example/mealer/Account.java
@@ -1,12 +1,6 @@
 package com.example.mealer;
 
 public interface Account {
-    String username = new String();
-    String firstName = new String();
-    String lastName = new String();
-    String email = new String();
-    String password = new String();
-    Boolean connected = false;
 
     public void connect(String email, String pswd);
     public void disconnect();
