9b58a5460b08ebd046d25c06889180c5244bf937
Amadou Cire Baba Ly <aly042@uottawa.ca>
2022-11-08 15:03:17 -0500

    firebase

--- app/src/main/java/com/example/mealer/dialogue.java
+++ app/src/main/java/com/example/mealer/dialogue.java
@@ -3,6 +3,7 @@ package com.example.mealer;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -103,6 +104,9 @@ public class dialogue extends AppCompatActivity {
     {
         datePickerDialog.show();
     }
-
+public void onListe(View view){
+    Intent intent = new Intent(getApplicationContext(), PlainteActivity.class);
+    startActivityForResult(intent, 0);
+}
 
 }
