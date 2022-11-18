69a109acc67b09714bb7fe99c71d2d569478e19d
voldigotcodes <vmonz093@uottawa.ca>
2022-11-18 10:49:59 -0500

    création des classes RepasModel et MenuModel + changement de meal_desc.java en repasMenuDuJourActivity pour plus de claireté

---
+++ app/src/main/java/com/example/mealer/MenuModel.java
@@ -0,0 +1,93 @@
+package com.example.mealer;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MenuModel {
+
+    ArrayList<RepasModel> menuArray;
+    ArrayList<RepasModel> menuDuJourArray;
+    private static MenuModel menu = new MenuModel();
+
+    String cuisinierId;
+
+    // databade reference and others
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private MenuModel(){
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Cuisinier");
+    }
+
+    public void addToMenu(RepasModel r){
+        assert !cuisinierId.isEmpty() && cuisinierId == null;
+
+        // Adding to the array
+        menuArray.add(r);
+
+        // Adding to the database
+        myRef = database.getReference("Cuisinier/" + cuisinierId + "/menu");
+
+        //Getting an id
+        String key = myRef.push().getKey();
+        r.setRepasId(key);
+        // Adding the repas to the database
+        myRef.child(key).setValue(r);
+
+
+
+    }
+
+    public void deleteFromMenu(RepasModel r){
+        assert !r.isInRepasDuJour();
+        assert !cuisinierId.isEmpty() && cuisinierId == null;
+
+        // Deleting from the Array
+        menuArray.remove(r);
+
+        // Deleting from the database
+        myRef = database.getReference("Cuisinier/" + cuisinierId + "/menu");
+
+        myRef.child(r.getRepasId()).removeValue();
+
+    }
+
+    public void addToRepasDujour(RepasModel r){
+        //TODO Implement this method
+        myRef = database.getReference("Cuisinier/" + cuisinierId + "/menu");
+        myRef.child(r.repasId).child("inRepasDuJour").setValue(true);
+        r.setInRepasDuJour("true");
+        menuDuJourArray.add(r);
+
+    }
+
+    public void deleteFromMenuDuJour(RepasModel r){
+        //TODO Implement this method
+
+    }
+
+    public void ShowMenu(){
+        //TODO Implement this method
+    }
+
+    public void ShowMenuDuJour(){
+        //TODO Implement this method
+
+    }
+
+    public String getCuisinierId(){
+        return cuisinierId;
+    }
+
+    public void setCuisinierId(String id){
+        cuisinierId = id;
+    }
+
+    private static MenuModel getInstance(){
+        return menu;
+    }
+
+}
---
+++ app/src/main/java/com/example/mealer/RepasModel.java
@@ -0,0 +1,98 @@
+package com.example.mealer;
+
+public class RepasModel {
+
+    String repasId, nom, typeDeRepas, typeDeCuisine,
+            ingredients, allergenes, description, inRepasDuJour;
+    double prix;
+
+
+
+    public RepasModel(){
+
+    }
+
+    public RepasModel(String nom, String typeDeRepas, String typeDeCuisine, String ingredients,
+                      String allergenes, Double prix, String description){
+        this.nom = nom;
+        this.typeDeRepas = typeDeRepas;
+        this.typeDeCuisine = typeDeCuisine;
+        this.ingredients = ingredients;
+        this.allergenes = allergenes;
+        this.prix = prix;
+        this.description = description;
+        this.inRepasDuJour = "false";
+
+    }
+
+    protected void setRepasId(String key){
+        repasId = key;
+    }
+
+    public String getRepasId(){
+        return repasId;
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTypeDeRepas() {
+        return typeDeRepas;
+    }
+
+    public String getTypeDeCuisine() {
+        return typeDeCuisine;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public String getAllergenes() {
+        return allergenes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setTypeDeRepas(String typeDeRepas) {
+        this.typeDeRepas = typeDeRepas;
+    }
+
+    public void setTypeDeCuisine(String typeDeCuisine) {
+        this.typeDeCuisine = typeDeCuisine;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setAllergenes(String allergenes) {
+        this.allergenes = allergenes;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setInRepasDuJour(String boolValue){
+        inRepasDuJour = boolValue;
+    }
+
+    public boolean isInRepasDuJour(){
+        return inRepasDuJour == "true";
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
