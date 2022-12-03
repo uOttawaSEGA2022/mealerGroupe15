0852adc4f2b0cabf5690e1feb15dd184ad37bb6b
voldigotcodes <vmonz093@uottawa.ca>
2022-12-03 11:26:59 -0500

    update fonction search pour que ca ne montre aucun repas quand on ecrit uelque chose qui n'appartient a aucun repas

--- app/src/main/java/com/example/mealer/ClientRechercheActivity.java
+++ app/src/main/java/com/example/mealer/ClientRechercheActivity.java
@@ -77,6 +77,7 @@ public class ClientRechercheActivity extends AppCompatActivity implements Recycl
             // if no item is added in filtered list we are
             // displaying a toast message as no data found.
             Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show();
+            adapter.filterList(filteredlist);
         } else {
             // at last we are passing that filtered
             // list to our adapter class.
